71f4cd6a1e27ad79aa702ae8252d0a9354d0f77f
placebo <r79166967650@yandex.ru>
2022-06-03 13:32:59 +0300

    modified:   script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo docker build -t metadata-extractor .
-sudo docker run --mount type=bind,source=$PWD/pdftest/Files,target=/app/pdftest/Files metadata-extractor
+chmod 777 script.sh
+docker build -t metadata-extractor .
+docker run --mount type=bind,source=$PWD/pdftest/Files,target=/app/pdftest/Files metadata-extractor
