f76d19a4cdba14e8499de8797e249939be9a219e
Fancy2000 <vip_dzhikaevi@mail.ru>
2022-06-03 16:12:18 +0300

    add docker push to dockerHub

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build -t metadata-extractor .
 docker run --mount type=bind,source=$PWD/pdftest/Files,target=/app/pdftest/Files metadata-extractor
+docker push metadata-extractor
